f78c2347d2c03d5984467279361a537808351b6d
Samira <s.fasahat.89@gmail.com>
2018-12-30 06:51:03 +0330

    remove comments lines

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -8,6 +8,7 @@ use App\Repositories\CategoryRepository;
 use App\Models\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
+// use \Validator;
 class CategoryController extends Controller
 {
 
@@ -29,41 +30,18 @@ class CategoryController extends Controller
     }
 
     public function store(Request $request) {
-        // private $rules = [
-        //     'title' => 'required|min:3'
-        // ];
-
-        // $validator = Validator::make($request->all(), [
-        //     'title' => 'required|min:3'
-            
-        // ]);
-        // return $validator;
-        // if ($validator->fails()) {
-        //     return response()->json(null,404);
-            
-        // } else {
-            // $category = Category::create($request->all());
-            // return response()->json($category, 201);
-        // }
-
-
-        // $valid = Category::validate($request->all());
         
-        // if ($valid->passes()) {
-        //   echo 'Everything is Valid!';
-        // } else {
-        //   var_dump($valid->messages());
-        // }
-        // die('');
-        // $validate = Category::validate($request->all());
+        $validator = $this->validate($request, [
+            'title' => 'required|max:3',
+        ]);
 
-        // if($validate) {
-            $category = Category::create($request->all());
+        if (!$validator)
+        {
+           $category = Category::create($request->all());
             return response()->json($category, 201);
-        // } else {
-        //     return response()->json(null,404);
-        // }
-        
+        } else {
+            return response()->json(null,204);
+        }     
     }
 
     public function update(Request $request,Category $category,$id){
@@ -74,11 +52,5 @@ class CategoryController extends Controller
     public function delete(Request $request,Category $category,$id) {
         $category->where('id', $id)->delete();
         return response()->json(null,204);
-    }
-
-    public function read() {
-        $jsondata = file_get_contents('test.json');
-        $json = json_decode($jsondata);
-        $id = $json->device->sn;
     }
 }
--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,12 +67,13 @@ return [
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'rest'),
+            'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
+            // 'schema'   => env('DB_PGSQL_SCHEMA','public'),
         ],
 
         'sqlsrv' => [
